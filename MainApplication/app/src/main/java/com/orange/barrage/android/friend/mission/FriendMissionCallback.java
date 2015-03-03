f2f0a86e0febf40c3442c943820200d9d8fab7df
Benson Peng <penglingzhe@gmail.com>
2015-03-03 17:33:47 +0800

    add files

---
+++ MainApplication/app/src/main/java/com/orange/barrage/android/friend/mission/FriendMissionCallback.java
@@ -0,0 +1,7 @@
+package com.orange.barrage.android.friend.mission;
+
+/**
+ * Created by pipi on 15/3/3.
+ */
+public interface FriendMissionCallback {
+}
---
+++ MainApplication/app/src/main/java/com/orange/barrage/android/friend/model/FriendManager.java
@@ -0,0 +1,11 @@
+package com.orange.barrage.android.friend.model;
+
+import javax.inject.Singleton;
+
+/**
+ * Created by pipi on 15/3/3.
+ */
+
+@Singleton
+public class FriendManager {
+}
