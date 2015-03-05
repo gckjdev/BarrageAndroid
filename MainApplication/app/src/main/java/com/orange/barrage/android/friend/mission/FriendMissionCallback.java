50378ad3752449bc798ffb60f644a6a949307144
Benson Peng <penglingzhe@gmail.com>
2015-03-05 12:09:13 +0800

    update

--- MainApplication/app/src/main/java/com/orange/barrage/android/friend/mission/FriendMissionCallback.java
+++ MainApplication/app/src/main/java/com/orange/barrage/android/friend/mission/FriendMissionCallback.java
@@ -4,4 +4,7 @@ package com.orange.barrage.android.friend.mission;
  * Created by pipi on 15/3/3.
  */
 public interface FriendMissionCallback {
+
+
+
 }
---
+++ MainApplication/app/src/main/java/com/orange/barrage/android/friend/model/TagManager.java
@@ -0,0 +1,7 @@
+package com.orange.barrage.android.friend.model;
+
+/**
+ * Created by pipi on 15/3/5.
+ */
+public class TagManager {
+}
