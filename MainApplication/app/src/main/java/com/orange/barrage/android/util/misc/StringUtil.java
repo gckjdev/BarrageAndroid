d41b8d868e9b937703af3a9a651c151c9bccebb2
Benson Peng <penglingzhe@gmail.com>
2015-03-05 15:08:01 +0800

    add encrypt password

--- MainApplication/app/src/main/java/com/orange/barrage/android/util/misc/StringUtil.java
+++ MainApplication/app/src/main/java/com/orange/barrage/android/util/misc/StringUtil.java
@@ -1,5 +1,10 @@
 package com.orange.barrage.android.util.misc;
 
+import com.loopj.android.http.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 public class StringUtil {
@@ -14,5 +19,40 @@ public class StringUtil {
 
     public static String createUUID() {
         return UUID.randomUUID().toString();
+    }
+
+    public static String md5base64encode(String input) {
+        try {
+            if (input == null)
+                return null;
+
+            if (input.length() == 0)
+                return null;
+
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes("UTF-8"));
+            byte[] enc = md.digest();
+            String base64str = Base64.encodeToString(enc, 0);
+            return base64str;
+
+        }
+        catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+        catch (UnsupportedEncodingException e) {
+            return null;
+        }
+
+    }
+
+    public static String base64encode(String input) {
+        if (input == null)
+            return null;
+
+        if (input.length() == 0)
+            return null;
+
+        String base64str = Base64.encodeToString(input.getBytes(), 0);
+        return base64str;
     }
 }
