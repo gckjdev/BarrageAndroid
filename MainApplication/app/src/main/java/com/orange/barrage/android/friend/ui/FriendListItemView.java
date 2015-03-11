794de027669e77de6818ae3fbeaac3e91d2367a0
xiaoshu <tudiheshifu@126.com>
2015-03-11 10:56:59 +0800

    update bg color

--- MainApplication/app/src/main/java/com/orange/barrage/android/friend/ui/FriendListItemView.java
+++ MainApplication/app/src/main/java/com/orange/barrage/android/friend/ui/FriendListItemView.java
@@ -3,7 +3,6 @@ package com.orange.barrage.android.friend.ui;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,8 +13,6 @@ import com.orange.protocol.message.UserProtos;
 
 import javax.inject.Inject;
 
-import roboguice.inject.InjectView;
-
 /**
  * Created by pipi on 15/3/10.
  */
@@ -25,8 +22,8 @@ public class FriendListItemView extends LinearLayout{
 
     private UserAvatarView mAvatarView;
 
-    public TextView mTextView1;
-    public TextView mTextView2;
+    public TextView friend_nick;
+    public TextView friend_signature;
     private View view;
     //凡是需要用户的时候，就设置这样一个用户变量,这样就以用户为单位进行操作了
     UserProtos.PBUser pbFriend;
@@ -58,13 +55,13 @@ public class FriendListItemView extends LinearLayout{
         this.pbFriend = friend;
 
         mAvatarView = (UserAvatarView)view.findViewById(R.id.friend_avatar_view);
-        mTextView1 = (TextView)view.findViewById(R.id.textview1);
-        mTextView2 = (TextView)view.findViewById(R.id.textview2);
+        friend_nick = (TextView)view.findViewById(R.id.friend_nick);
+        friend_signature = (TextView)view.findViewById(R.id.friend_signature);
 
         String nick = pbFriend.getNick();
         String signature = pbFriend.getSignature();
-        mTextView1.setText(nick);
-        mTextView2.setText(signature);
+        friend_nick.setText(nick);
+        friend_signature.setText(signature);
 
 
         mAvatarView.loadUser(pbFriend);
