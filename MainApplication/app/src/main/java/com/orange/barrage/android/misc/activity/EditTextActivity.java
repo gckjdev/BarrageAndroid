44f70057c94f785b9d834fab5e4ab9a14528fc00
xiaoshu <tudiheshifu@126.com>
2015-03-18 15:15:36 +0800

    Add The Person Nick And Signature

--- MainApplication/app/src/main/java/com/orange/barrage/android/misc/activity/EditTextActivity.java
+++ MainApplication/app/src/main/java/com/orange/barrage/android/misc/activity/EditTextActivity.java
@@ -6,8 +6,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.orange.barrage.android.R;
+import com.orange.barrage.android.user.model.UserManager;
 import com.orange.barrage.android.util.activity.ActivityIntent;
 import com.orange.barrage.android.util.activity.BarrageCommonActivity;
+import com.orange.protocol.message.UserProtos;
+
+import javax.inject.Inject;
 
 import roboguice.inject.InjectView;
 
@@ -18,9 +22,14 @@ public class EditTextActivity extends BarrageCommonActivity{
 
     @InjectView(R.id.nickdescription)
     private TextView mNickdescription;
+    //获取本地用户只需要两步,一步是取得用户管理，第二是getUser
+    @Inject
+    UserManager mUserManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState,R.layout.activity_edit_text,"",R.string.y_tijiao);
+        UserProtos.PBUser user = mUserManager.getUser();
+        mClearEdittext.setText(user.getNick());
     }
 
     public static void start(Activity fromActivity,
