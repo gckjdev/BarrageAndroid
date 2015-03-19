89205440db4883adbf515474356b0eb8e23a9d45
xiaoshu <tudiheshifu@126.com>
2015-03-19 10:04:20 +0800

    add the modify password logical

--- MainApplication/app/src/main/java/com/orange/barrage/android/user/ui/user_home/UserHomeModifyPasswordActivity.java
+++ MainApplication/app/src/main/java/com/orange/barrage/android/user/ui/user_home/UserHomeModifyPasswordActivity.java
@@ -1,12 +1,14 @@
 package com.orange.barrage.android.user.ui.user_home;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 
 import com.orange.barrage.android.R;
 import com.orange.barrage.android.user.ui.view.InputEditText;
 import com.orange.barrage.android.util.activity.BarrageCommonActivity;
+import com.orange.barrage.android.util.activity.MessageCenter;
 
 import roboguice.inject.InjectView;
 
@@ -25,7 +27,14 @@ public class UserHomeModifyPasswordActivity extends BarrageCommonActivity {
         mSubmitPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (TextUtils.isEmpty(mInputNewPassword.getText().toString())||(TextUtils.isEmpty(mInoutConfirmPassword.getText().toString())))
+                {
+                    MessageCenter.postErrorMessage("新密码不能为空");
+                }
+                else if (!mInputNewPassword.getText().toString().equals(mInoutConfirmPassword.getText().toString()))
+                {
+                    MessageCenter.postErrorMessage("新旧密码不一样，请重新输入");
+                }
             }
         });
     }
